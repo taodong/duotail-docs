a4c26bd9991d132637f9e37ea79c366b63471d62
Tao Dong <taojdcn@gmail.com>
2025-12-22 22:37:28 -0800

    First working version

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,8 @@
 import clsx from 'clsx';
 import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import {Redirect} from '@docusaurus/router';
 import Layout from '@theme/Layout';
-import HomepageFeatures from '@site/src/components/HomepageFeatures';
 
 import Heading from '@theme/Heading';
 import styles from './index.module.css';
@@ -29,15 +29,5 @@ function HomepageHeader() {
 }
 
 export default function Home() {
-  const {siteConfig} = useDocusaurusContext();
-  return (
-    <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
-      <HomepageHeader />
-      <main>
-        <HomepageFeatures />
-      </main>
-    </Layout>
-  );
+  return <Redirect to="/welcome" />;
 }
